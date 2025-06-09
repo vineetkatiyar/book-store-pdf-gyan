7bf78ae26c6e3784bd265aaa49a56f0c0a5ecbac
Vineet Katiyar <vineetkatiyar98@gmail.com>
2025-06-09 22:41:28 +0530

    creating book model

---
+++ backend/src/books/book.model.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { BookType } from "./bookType";
+
+const bookSchema = new mongoose.Schema<BookType>({
+    title : {
+        type : String,
+        required: true,
+        trim: true,
+    },
+
+    author : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    genre : {
+        type : String,
+        required: true,
+        trim: true,
+    },
+    coverImageUrl : {
+        type : String,
+        required: true,
+        trim: true,
+    },
+    file : {
+        type : String,
+        required: true,
+        trim: true,
+    },
+},{
+    timestamps: true,
+})
+
+const book = mongoose.model<BookType>("Book", bookSchema);
+export default book;
---
+++ backend/src/books/bookType.ts
@@ -0,0 +1,12 @@
+import { User } from "../user/user.types";
+
+export interface BookType {
+    _id : string;
+    title: string;
+    author : User;
+    genre : string;
+    coverImageUrl : string;
+    file : string;
+    createAt : Date;
+    updatedAt : Date;
+}
